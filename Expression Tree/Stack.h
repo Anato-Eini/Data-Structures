f392491fa8d8c611a60374292aa671d88e93692b
Anato-Eini <135421389+Anato-Eini@users.noreply.github.com>
2023-11-16 00:39:40 +0800

    Update Stack.h

--- Expression Tree/Stack.h
+++ Expression Tree/Stack.h
@@ -3,10 +3,14 @@
 #include <iostream>
 using namespace std;
 class StackOp: public Stack{
-    TreeNode* head;
+    TreeNode* head; // hello my sentinel
+
+    //check if empty
     bool isEmpty(){
         return head->next == nullptr;
     }
+
+    //self-explanatory nani diri nga part
     int evaluate(int number1, char op, int number2){
         switch (op) {
             case '+':
@@ -22,13 +26,18 @@ class StackOp: public Stack{
         }
     }
 public:
+    //constructor
     StackOp(){
         head->next = nullptr;
     }
+
+    //create node first then point its next to the next of head since we're using sentinel
     void push(TreeNode* newNode){
         newNode->next = head->next;
         head->next = newNode;
     }
+
+    //we're dealing with nodes in a tree, we'll just return the node to reuse its address pointer
     TreeNode* pop(){
         if(!isEmpty()){
             TreeNode* out = head->next;
@@ -39,6 +48,8 @@ public:
             return nullptr;
         }
     }
+
+    //printing tree from root to its leaves using inorder traversal
     void printTree(TreeNode* n){
         if(n != nullptr){
             printTree(n->left);
@@ -46,9 +57,12 @@ public:
             printTree(n->right);
         }
     }
+
+    //evaluate tree 
+    //explanation: if current node contains a number then return that number, otherwise, go to the left child and right child
     int evaluateTree(TreeNode* n){
         if(isdigit(n->value)){
-            return atoi(&n->value);
+            return atoi(&n->value);//atoi -> character to integer from #include<string>
         }else{
             return evaluate(evaluateTree(n->left), n->value, evaluateTree(n->right));
         }
