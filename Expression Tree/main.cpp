7a5f1da8104b8dcf31a64b7233f783cb7ca41149
Anato-Eini <135421389+Anato-Eini@users.noreply.github.com>
2023-11-16 00:52:00 +0800

    Update main.cpp

--- Expression Tree/main.cpp
+++ Expression Tree/main.cpp
@@ -10,8 +10,8 @@ int main(){
     for(char a: input){
         /*
         Explanation: If ang current value is an operator, create new node -> pop the stack twice and make it the left child
-        and right child respectively of the new node, otherwise if number, then create new Node assign nullpointers to its children
-        then push it back to stack
+        and right child respectively of the new node and push it back to stack, otherwise if number, then create new Node assign nullpointers to its children
+        and push it back to stack
         */
         if(a == '+' || a == '-' || a == '/' || a == '*' || a == '%'){
             TreeNode *c = stack->pop(), *b = stack->pop();
