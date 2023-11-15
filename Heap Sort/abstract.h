b1a9d68716df8910acce198b0793a27d31aa18a1
Anato-Eini <135421389+Anato-Eini@users.noreply.github.com>
2023-11-15 12:05:58 +0800

    Add files via upload

---
+++ Heap Sort/abstract.h
@@ -0,0 +1,4 @@
+class AbstractHeap{
+public:
+    virtual void heapSort(int arr[], int n) = 0;
+};
--- Heap Sort/main.cpp
+++ Heap Sort/main.cpp
@@ -1,42 +1,9 @@
 #include <iostream>
+#include "Heap.h"
 using namespace std;
 
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
-void heapify(int arr[], int n, int i) {
-    int largest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
-    if (left < n && arr[left] > arr[largest]) {
-        largest = left;
-    }
-    if (right < n && arr[right] > arr[largest]) {
-        largest = right;
-    }
-    if (largest != i) {
-        swap(arr[i], arr[largest]);
-        heapify(arr, n, largest);
-    }
-}
-
-void buildHeap(int arr[], int n) {
-    for (int i = n / 2 - 1; i >= 0; i--) {
-        heapify(arr, n, i);
-    }
-}
-void heapSort(int arr[], int n) {
-    buildHeap(arr, n);
-    for (int i = n - 1; i >= 0; i--) {
-        swap(arr[0], arr[i]);
-        heapify(arr, i, 0);
-    }
-}
-
 int main() {
+    Heap* heap = new Heap();
     int size;
     cout << "Enter the size of the array: ";
     cin >> size;
@@ -45,7 +12,7 @@ int main() {
         cout << "Enter element " << a + 1 << ": ";
         cin >> array[a];
     }
-    heapSort(array, size);
+    heap->heapSort(array, size);
     for(int a = 0; a < size; a++){
         cout << array[a] << " ";
     }
