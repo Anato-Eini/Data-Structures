efbcd6e3c4ca651c6b32941c86b1cb531d759fed
Anato-Eini <jameskennethacabal@gmail.com>
2024-03-21 07:24:22 +0800

    Splay Tree Partial

---
+++ Splay Tree/Node.h
@@ -0,0 +1,9 @@
+#ifndef TREES_AND_HEAPS_NODE_H
+#define TREES_AND_HEAPS_NODE_H
+
+struct Node{
+    int value;
+    Node* left, *right, *parent;
+};
+
+#endif //TREES_AND_HEAPS_NODE_H
--- Splay Tree/SplayTree.h
+++ Splay Tree/SplayTree.h
@@ -1,9 +1,34 @@
 #ifndef TREES_AND_HEAPS_SPLAYTREE_H
 #define TREES_AND_HEAPS_SPLAYTREE_H
-
+#include <iostream>
+using namespace std;
+#include "Node.h"
 
 class SplayTree {
-
+    Node* root;
+    int size;
+    static Node* minimumValue(Node*);
+    Node* searchNodeHelper(Node*, int);
+    static Node* createNode(Node*, int);
+    void zag(Node*);
+    void zig(Node*);
+    void insertHelper(Node*, int);
+    void deleteNodeHelper(Node*);
+    void insertFix(Node*, int);
+    void deleteFix(Node*);
+    void nodeTransplant(Node*, Node*);
+    void printTreeHelper(Node*, string, bool);
+    static int nodeHeight(Node*);
+public:
+    SplayTree(): root(nullptr), size(0){}
+    Node* searchNode(int);
+    bool isEmpty();
+    void insertNode(int);
+    void deleteNode(int);
+    void printTree();
+    int depth(int);
+    int height(int);
+    int treeHeight();
 };
 
 
