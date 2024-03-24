3668645d074f890222366a02ab09886e92be1673
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-03-25 07:44:29 +0800

    Splay method

--- Splay Tree/SplayTree.cpp
+++ Splay Tree/SplayTree.cpp
@@ -35,3 +35,44 @@ void SplayTree::rightRotate(Node* node){
     if(leftRightNode)
         leftRightNode->parent = node;
 }
+
+void SplayTree::splay(int value) {
+    splayHelper(root, value);
+}
+
+void SplayTree::splayHelper(Node *node, int value) {
+    if(!node || node->value == value)
+        return;
+
+    if(node->value > value){
+        if(!node->left)
+            return;
+
+        if(node->left->value > value){
+            splayHelper(node->left->left, value);
+            rightRotate(node);
+            splayHelper(node->parent, value);
+        }else if(node->left->value < value){
+            splayHelper(node->left->right, value);
+            if(node->left->right)
+                leftRotate(node->left);
+            splayHelper(node, value);
+        }else
+            rightRotate(node->left);
+    }else{
+        if(!node->right)
+            return;
+
+        if(node->right->value < value){
+            splayHelper(node->right->right, value);
+            leftRotate(node);
+            splayHelper(node->parent, value);
+        }else if(node->right->value > value){
+            splayHelper(node->right->left, value);
+            if(node->right->left)
+                rightRotate(node->right);
+            splayHelper(node, value);
+        }else
+            leftRotate(node->left);
+    }
+}
