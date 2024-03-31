c6c88cc2ffd673af77cfb558f05eca26892b178a
Destiny <jameskennethacabal@gmail.com>
2024-03-31 23:27:40 +0800

    Revisions

---
+++ Binary Search Tree/BinarySearchTree.h
@@ -0,0 +1,40 @@
+#ifndef DATA_STRUCTURES_BINARYSEARCHTREE_H
+#define DATA_STRUCTURES_BINARYSEARCHTREE_H
+
+#include "Node.h"
+#include <iostream>
+
+using namespace std;
+class BinarySearchTree {
+    int size;
+    Node* root;
+    Node* createNode(int, Node*);
+    Node* searchNodeHelper(Node*, int);
+    Node* minimumNode(Node*);
+    void insertNodeHelper(Node*, int);
+    void deleteNodeHelper(Node*);
+    void nodeTransplant(Node*, Node*);
+    void printTreeHelper(Node*, string, bool);
+    void printInorderHelper(Node*);
+    void printPostOrderHelper(Node*);
+    void printPreOrderHelper(Node*);
+    int depthNodeHelper(Node*, int);
+    int heightNodeHelper(Node*);
+public:
+    BinarySearchTree();
+    void insertNode(int);
+    void searchNode(int);
+    void deleteNode(int);
+    void sibling(int);
+    int treeHeight();
+    int heightNode(int);
+    int depthNode(int);
+    void printTree();
+    void printInorder();
+    void printPostOrder();
+    void printPreOrder();
+    void printBFS();
+};
+
+
+#endif //DATA_STRUCTURES_BINARYSEARCHTREE_H
