e86f5971718c72ef5743af2e7ef2f6e01a34cfef
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-04-07 19:08:29 +0800

    README.md

--- AVL Tree/main.cpp
+++ AVL Tree/main.cpp
@@ -1,9 +1,3 @@
-/*
- * I implemented the AVL Tree because of the reason that I was amazed at this visualization. I was merely curious until
- * I got the urge to implement it by myself.
- * https://www.youtube.com/watch?v=mGF61O21W-c
- * A little bit complex and "juggly" but that's what makes it so fun and challenging
- */
 #include <iostream>
 #include "AVLTree.h"
 using namespace std;
