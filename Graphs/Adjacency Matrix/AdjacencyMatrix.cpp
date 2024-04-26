14fdbbbeba860ed7ccc952fa80d51b79d569e479
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-04-27 07:25:04 +0800

    Adjacency Matrix

---
+++ Graphs/Adjacency Matrix/AdjacencyMatrix.cpp
@@ -0,0 +1,73 @@
+#include "AdjacencyMatrix.h"
+
+std::vector<std::string> AdjacencyMatrix::vertices() const {
+    return std::vector<std::string>();
+}
+
+std::vector<std::string> AdjacencyMatrix::edges() const {
+    return std::vector<std::string>();
+}
+
+std::pair<std::string, std::string> AdjacencyMatrix::endVertices(const std::string &edge) {
+    return std::pair<std::string, std::string>();
+}
+
+std::vector<std::string> AdjacencyMatrix::outgoingEdges(const std::string &vertex) {
+    return std::vector<std::string>();
+}
+
+std::vector<std::string> AdjacencyMatrix::incomingEdges(const std::string &vertex) {
+    return std::vector<std::string>();
+}
+
+std::string AdjacencyMatrix::getEdge(const std::string &vertex1, const std::string &vertex2) {
+    return std::string();
+}
+
+std::vector<std::string> AdjacencyMatrix::opposite(const std::string &vertex) {
+    return std::vector<std::string>();
+}
+
+void AdjacencyMatrix::addVertex(const std::string &vertex) {
+
+}
+
+void AdjacencyMatrix::addEdge(const std::string &edge, const std::string &vertex1, const std::string &vertex2) {
+
+}
+
+void AdjacencyMatrix::removeVertex(const std::string &vertex) {
+
+}
+
+void AdjacencyMatrix::removeEdge(const std::string &edge) {
+
+}
+
+bool AdjacencyMatrix::containEdge(const std::string &edge) const {
+    return false;
+}
+
+bool AdjacencyMatrix::containVertex(const std::string &vertex) const {
+    return false;
+}
+
+int AdjacencyMatrix::numVertices() {
+    return 0;
+}
+
+int AdjacencyMatrix::numEdges() {
+    return 0;
+}
+
+int AdjacencyMatrix::outDegree(const std::string &vertex) {
+    return 0;
+}
+
+int AdjacencyMatrix::inDegree(const std::string &vertex) {
+    return 0;
+}
+
+void AdjacencyMatrix::print(std::ostream &ostream) {
+
+}
---
+++ Graphs/Adjacency Matrix/AdjacencyMatrix.h
@@ -0,0 +1,46 @@
+#ifndef DATA_STRUCTURES_ADJACENCYMATRIX_H
+#define DATA_STRUCTURES_ADJACENCYMATRIX_H
+
+#include "../General Graph/Graph.h"
+
+class AdjacencyMatrix : public Graph{
+    void print(std::ostream &ostream) override;
+
+public:
+    std::vector<std::string> vertices() const override;
+
+    std::vector<std::string> edges() const override;
+
+    std::pair<std::string, std::string> endVertices(const std::string &edge) override;
+
+    std::vector<std::string> outgoingEdges(const std::string &vertex) override;
+
+    std::vector<std::string> incomingEdges(const std::string &vertex) override;
+
+    std::string getEdge(const std::string &vertex1, const std::string &vertex2) override;
+
+    std::vector<std::string> opposite(const std::string &vertex) override;
+
+    void addVertex(const std::string &vertex) override;
+
+    void addEdge(const std::string &edge, const std::string &vertex1, const std::string &vertex2) override;
+
+    void removeVertex(const std::string &vertex) override;
+
+    void removeEdge(const std::string &edge) override;
+
+    bool containEdge(const std::string &edge) const override;
+
+    bool containVertex(const std::string &vertex) const override;
+
+    int numVertices() override;
+
+    int numEdges() override;
+
+    int outDegree(const std::string &vertex) override;
+
+    int inDegree(const std::string &vertex) override;
+};
+
+
+#endif //DATA_STRUCTURES_ADJACENCYMATRIX_H
