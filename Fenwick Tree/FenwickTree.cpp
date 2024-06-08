530da70d94a91c1989be7c62b6cec7ad01637ef4
Destiny <jameskennethacabal@gmail.com>
2024-06-08 14:50:18 +0800

    Fenwick Tree

--- Fenwick Tree/FenwickTree.cpp
+++ Fenwick Tree/FenwickTree.cpp
@@ -1,6 +1,90 @@
 #include "FenwickTree.h"
 
-FenwickTree::FenwickTree(int size) : array(new int[size]){}
+FenwickTree::FenwickTree(const int & size) : array(new int[size + 1]), size(size + 1)
+{
+    for(int i = 0; i <= size; i++)
+        array[i] = 0;
+}
+
+FenwickTree::FenwickTree() : array(nullptr), size(0) {}
+
+void FenwickTree::print(std::ostream& ostream) const
+{
+    bool isFirst = true;
+    ostream << "[ ";
+    for(int i = 1; i < size; i++)
+    {
+        if(!isFirst)
+            ostream << ", ";
+        else
+            isFirst = false;
+
+        ostream << array[i];
+    }
+    ostream << " ]\n";
+}
+
+
+void FenwickTree::buildArray(const int & size)
+{
+    if(array)
+        clearArray();
+
+    array = new int[size + 1];
+    for(int i = 0; i <= size; i++)
+        array[i] = 0;
+
+    this->size = size + 1;
+}
+
+
+FenwickTree& FenwickTree::buildTree(const int array[], const int& n)
+{
+    buildArray(n);
+
+    for(int i = 0; i < n; i++)
+        for(int j = i + 1; j <= n; j += j & -j)
+            this->array[j] += array[i];
+
+    return *this;
+}
+
+FenwickTree& FenwickTree::buildTree(const std::vector<int>& array)
+{
+    const int size = static_cast<int>(array.size());
+    buildArray(size);
+
+    for(int i = 0; i < size; i++)
+        for(int j = i + 1; j <= size; j += j & -j)
+            this->array[j] += array[i];
+
+    return *this;
+}
+
+void FenwickTree::update_tree(int index, int && value) const
+{
+    for(++index; index < size; index += index & -index)
+        array[index] += value;
+}
+
+FenwickTree& FenwickTree::updateArray(int array[], const int& index, const int& value)
+{
+    const int prevValue = array[index];
+    array[index] = value;
+
+    update_tree(index, value - prevValue);
+    return *this;
+}
+
+
+int FenwickTree::getSum(int index) const
+{
+    int sum = 0;
+    for(++index; index > 0; index -= index & -index)
+        sum += array[index];
+
+    return sum;
+}
 
 FenwickTree & FenwickTree::clearArray() {
     delete[] array;
@@ -9,5 +93,12 @@ FenwickTree & FenwickTree::clearArray() {
 }
 
 FenwickTree::~FenwickTree(){
-    delete[] array;
+    clearArray();
+}
+
+std::ostream & operator<<(std::ostream & ostream, const FenwickTree * tree)
+{
+    tree->print(ostream);
+
+    return ostream;
 }
--- Fenwick Tree/FenwickTree.h
+++ Fenwick Tree/FenwickTree.h
@@ -1,16 +1,51 @@
 #ifndef FENWICKTREE_H
 #define FENWICKTREE_H
+#include <ostream>
+#include <vector>
 
-class FenwickTree {
+class FenwickTree final {
     int* array;
+    int size;
+
+    void buildArray(const int & size);
+
+    void print(std::ostream & ostream) const;
+
+    void update_tree(int index, int && value) const;
 
 public:
-    explicit FenwickTree(int size);
+    explicit FenwickTree(const int & size);
+
+    FenwickTree();
 
     FenwickTree & clearArray();
 
+    FenwickTree & buildTree(const int array[], const int & n);
+
+    FenwickTree & buildTree(const std::vector<int> & array);
+
+    /**
+     * Must be called whenever there's a change in the original array[i] to update the tree instead of updating the
+     * array[i] directly
+     * @param array original array
+     * @param index index of value to be change
+     * @param value new value
+     * @return tree itself;
+     */
+    FenwickTree & updateArray(int array[], const int & index, const int & value);
+
+    /**
+     *
+     * @param index end index(inclusive)
+     * @return get sum from 0 up to end index;
+     */
+    [[nodiscard]] int getSum(int index) const;
+
     ~FenwickTree();
 
+    friend std::ostream & operator<<(std::ostream & ostream, const FenwickTree * tree);
 };
+
+std::ostream & operator<<(std::ostream & ostream, const FenwickTree * tree);
 
 #endif //FENWICKTREE_H
