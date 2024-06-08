3dc09dba0f9c33334848f45968793e8ef8fc89a7
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-06-08 12:27:34 +0800

    Partial Fenwick

--- Fenwick Tree/FenwickTree.cpp
+++ Fenwick Tree/FenwickTree.cpp
@@ -1,1 +1,13 @@
 #include "FenwickTree.h"
+
+FenwickTree::FenwickTree(int size) : array(new int[size]){}
+
+FenwickTree & FenwickTree::clearArray() {
+    delete[] array;
+
+    return *this;
+}
+
+FenwickTree::~FenwickTree(){
+    delete[] array;
+}
--- Fenwick Tree/FenwickTree.h
+++ Fenwick Tree/FenwickTree.h
@@ -1,10 +1,16 @@
 #ifndef FENWICKTREE_H
 #define FENWICKTREE_H
-#include <vector>
 
 class FenwickTree {
+    int* array;
 
 public:
+    explicit FenwickTree(int size);
+
+    FenwickTree & clearArray();
+
+    ~FenwickTree();
+
 };
 
 #endif //FENWICKTREE_H
