7661ce3940bad1db018784623550e53f552eaaca
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-07-30 22:02:33 +0800

    Partial

---
+++ Skip List/SkipList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by HP on 7/30/2024.
+//
+
+#include "SkipList.h"
---
+++ Skip List/SkipList.h
@@ -0,0 +1,14 @@
+//
+// Created by HP on 7/30/2024.
+//
+
+#ifndef DATA_STRUCTURES_SKIPLIST_H
+#define DATA_STRUCTURES_SKIPLIST_H
+
+
+class SkipList {
+
+};
+
+
+#endif //DATA_STRUCTURES_SKIPLIST_H
