8bbad4cc853dcdcc7a7444f29fdea21cc9bfaaab
Fleurette <135421389+Anato-Eini@users.noreply.github.com>
2024-08-06 18:32:06 +0800

    Insertion

--- K-d Tree/Kd_Tree.cpp
+++ K-d Tree/Kd_Tree.cpp
@@ -4,14 +4,125 @@ Kd_Tree::Kd_Tree() {
     Kd_Tree(1);
 }
 
-Kd_Tree::Node::Node(int *point) {
+Kd_Tree::Node::Node(const int point[]) {
+    const size_t sizeArray = sizeof(*point) / sizeof(point[0]);
+    this->point = new int[sizeArray];
+    for(size_t i = 0; i < sizeArray; i++)
+        this->point[i] = point[i];
 
+    left = right = nullptr;
+}
+
+Kd_Tree::Node::Node(const std::vector<int> & point){
+    const size_t sizeArray = point.size();
+    this->point = new int[sizeArray];
+    for(size_t i = 0; i < sizeArray; i++)
+        this->point[i] = point[i];
+
+    left = right = nullptr;
+}
+
+Kd_Tree::Node::~Node(){
+    delete point;
+    delete right;
+    delete left;
 }
 
 Kd_Tree::Kd_Tree(const int k_dimension) : root{nullptr}, size{0}, k_dimension{k_dimension} {}
 
-void Kd_Tree::insertKey(int point[]) {
-    if(!root){
-        root = new Node
+void Kd_Tree::insertKey(const int point[]) {
+    size_t pointSize = sizeof(*point) / sizeof(point[0]);
+    if (pointSize != k_dimension)
+        return;
+
+    if(!root)
+        root = new Node{point};
+    else{
+        Node * curr = root;
+
+        for(size_t i = 0; i < k_dimension; i++)
+            if(point[i % k_dimension] < curr->point[i % k_dimension]){
+                if (curr->left)
+                    curr = curr->left;
+                else{
+                    curr->left = new Node{point};
+                    break;
+                }
+            } else {
+                int currPoint = curr->point[i % k_dimension];
+                int pParameter = point[i % k_dimension];
+
+                if(i == k_dimension - 1 && currPoint == pParameter)
+                    return;
+                else if(curr->right)
+                    curr = curr->right;
+                else{
+                    curr->right = new Node{point};
+                    break;
+                }
+            }
     }
+
+    size++;
+}
+
+void Kd_Tree::insertKey(const std::vector<int> & point) {
+    size_t pointSize = point.size();
+    if (pointSize != k_dimension)
+        return;
+
+    if(!root)
+        root = new Node{point};
+    else{
+        Node * curr = root;
+
+        for(size_t i = 0; i < k_dimension; i++)
+            if(point[i % k_dimension] < curr->point[i % k_dimension]){
+                if (curr->left)
+                    curr = curr->left;
+                else{
+                    curr->left = new Node{point};
+                    break;
+                }
+            } else {
+                int currPoint = curr->point[i % k_dimension];
+                int pParameter = point[i % k_dimension];
+
+                if(i == k_dimension - 1 && currPoint == pParameter)
+                    return;
+                else if(curr->right)
+                    curr = curr->right;
+                else{
+                    curr->right = new Node{point};
+                    break;
+                }
+            }
+    }
+
+    size++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Kd_Tree::print(std::ostream & ostream) const {
+
 }
--- K-d Tree/Kd_Tree.h
+++ K-d Tree/Kd_Tree.h
@@ -1,24 +1,40 @@
 #ifndef DATA_STRUCTURES_KD_TREE_H
 #define DATA_STRUCTURES_KD_TREE_H
 
+#include "vector"
+#include "ostream"
 
 class Kd_Tree {
+
     struct Node{
         int * point;
-        Node * next;
-        Node * prev;
 
-        Node(int point[]);
+        Node * right;
+
+        Node * left;
+
+        explicit Node(const int point[]);
+
+        explicit Node(const std::vector<int> & point);
+
+        ~Node();
     };
+
+    void print(std::ostream & ostream, Node * node, std::string && indent, bool isLeaf) const;
 
 public:
     Kd_Tree();
 
-    Kd_Tree(const int k_dimension);
+    explicit Kd_Tree(const int k_dimension);
 
-    void insertKey(int point[]);
+    void insertKey(const int point[]);
 
-    void searchKey(int point[]);
+    void insertKey(const std::vector<int> & point);
+
+    void searchKey(const int point[]) const;
+
+    void searchKey(const std::vector<int> & point) const;
+
 
 private:
     //Fields
@@ -28,7 +44,22 @@ private:
     int size;
 
     int k_dimension;
+
+    friend std::ostream & operator<<(std::ostream & ostream, const Kd_Tree * tree);
+
+    friend std::ostream & operator<<(std::ostream & ostream, const Kd_Tree & tree);
 };
 
+std::ostream & operator<<(std::ostream & ostream, const Kd_Tree * tree){
+    tree->print(ostream);
+
+    return ostream;
+}
+
+std::ostream & operator<<(std::ostream & ostream, const Kd_Tree & tree){
+    tree.print(ostream);
+
+    return ostream;
+}
 
 #endif //DATA_STRUCTURES_KD_TREE_H
